f8c635cbc96bec99cdf59bdc398ef0c1b602fe25
James <james.hou.23@bishops.com>
2021-04-26 11:32:48 -0700

    login logout done

--- teamApp/views.py
+++ teamApp/views.py
@@ -4,41 +4,64 @@ from django.views import View
 from .models import Profile, Team, Tag
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 loggedIn = True
 
-# Create your views here.
 class IndexView(View):
     allTeams = Team.objects.order_by("-members_needed")
 
     def get(self, request):
+        user = request.user
         form = AuthenticationForm()
-        users = User.objects.all()
-
         context = {
-            'form':form,
             'allTeams': self.allTeams,
-            'user': request.user,
-            'users': users
+            'form': form,
+            'user': user
         }
+
         return render(request, 'teamApp/index.html', context)
 
     def post(self, request):
-        pass
+        if 'logout' in request.POST.keys():
+            logout(request)
+            form = AuthenticationForm()
+        else:
+            form = AuthenticationForm(data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password']
+                user = authenticate(username = username, password = password)
+                if user is not None:
+                    login(request, user = user)
+
+        context = {
+            'form': form,
+            'allTeams': self.allTeams,
+        }
+
+        return render(request, "teamApp/index.html", context)
 
 class ProfileView(View):
     def get(self, request, usr):
         user = User.objects.get(username = usr)
         tags = user.tag_set.all()
-        profile = get_object_or_404(Profile, pk=user)
-        context = {
-            'username': usr,
-            'profile': profile,
-        }
+        if request.user.username == usr:
+            # if the user is the profile owner
+            pass
+        else:
+            if request.user.is_authenticated:
+                # if the viewer is logged in but not page owner
+                pass
+            else:
+                # if the viewer is not logged in
+                pass
+        # profile = get_object_or_404(Profile, pk=user)
+        # context = {
+        #     'username': usr,
+        #     'profile': profile,
+        # }
         return render(request, 'teamApp/profile.html', context)
-
-    def post(self, request):
-        pass
 
 class EditProfileView(View):
     pass
@@ -50,12 +73,13 @@ class MyTeamsView(View):
 class TeamView(View):
     def get(self, request, team_id):
         team = get_object_or_404(Team, pk=team_id)
-        # members = User.objects.filter(username=team.members)
-        # members = team.user_set.all()
-        # print(members)
+        members = team.user_set.all()
+        if request.user in members:
+            pass
+        else:
+            pass
         context = {
-            'team':team,
-            # 'members': members
+            'team':team
         }
         return render(request, 'teamApp/team.html', context)
 
@@ -67,8 +91,15 @@ class EditTeamView(View):
     pass
 
 class CreateView(View):
-    # return HttpResponse("hello, create your team here")
-    pass
+    def get(self, request):
+        user = request.user
+        if user.is_authenticated:
+            pass
+        else:
+            pass
+
+    def post(self, request):
+        pass
 
 class RecView(View):
     pass
